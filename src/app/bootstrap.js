6791ed9e28381f73cfdaf50f6079dd847a999a2d
Daniel Olano <danielolanoc@gmail.com>
2015-04-11 10:02:49 -0500

    feat(boot): check browser compatibility
    closes #12

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -11,15 +11,33 @@ export function configure(aurelia) {
         .router()
         .eventAggregator();
 
-    // boot actions
-    Promise.all([
-        aurelia.start(),
-        wait(MIN_WAIT)
-    ]).then(r => r[0].setRoot('yi/app'));
+    if (compatibleBrowser())
+        // boot actions
+        Promise.all([
+            aurelia.start(),
+            wait(MIN_WAIT)
+        ]).then(r => r[0].setRoot('yi/app'));
+    else
+        showCompatMessage();
 }
 
 function wait(seconds) {
     return new Promise(resolve => {
         window.setTimeout(resolve, seconds * 1000);
+    });
+}
+
+function compatibleBrowser() {
+    // is chrome
+    return navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+}
+
+function showCompatMessage() {
+    const WAIT = 1;
+    wait(WAIT).then(()=> {
+        var compatMessage = document.querySelector('#boot>article'),
+            logo = document.querySelector('#logo');
+        compatMessage.classList.add('show');
+        logo.classList.remove('loading');
     });
 }
