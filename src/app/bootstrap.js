87a9b7f1fb66ef294eb7e291b461e8e9bbafdc4c
Daniel Olano <danielolanoc@gmail.com>
2015-04-10 19:24:31 -0500

    feat(boot): start loading animation
    closes #11

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -3,11 +3,23 @@
  * @param aurelia
  */
 export function configure(aurelia) {
+    const MIN_WAIT = 2;
+
     aurelia.use
         .defaultBindingLanguage()
         .defaultResources()
         .router()
         .eventAggregator();
 
-    aurelia.start().then(a => a.setRoot('yi/app'));
+    // boot actions
+    Promise.all([
+        aurelia.start(),
+        wait(MIN_WAIT)
+    ]).then(r => r[0].setRoot('yi/app'));
+}
+
+function wait(seconds) {
+    return new Promise(resolve => {
+        window.setTimeout(resolve, seconds * 1000);
+    });
 }
