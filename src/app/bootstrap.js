02e942bcf4b7df80f56d99ac66a4bb41f33a2fb8
Daniel Olano <danielolanoc@gmail.com>
2015-04-12 10:31:01 -0500

    fix(boot): compatibility message not showing

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -1,9 +1,32 @@
+// minimum time for the loading animation
+const MIN_WAIT = 2.5; // seconds
+var minWait = wait(MIN_WAIT);
+// bootstrap on module load
+bootstrap();
+
 /**
- * Configure and bootstrap aurelia application
+ * Check browser compatibility and run application
+ */
+function bootstrap() {
+    // load webcomponents polyfill only if needed
+    // TODO: https://github.com/webcomponents/webcomponentsjs/issues/283
+    // var webComp = supportsWC() ? Promise.resolve() : System.import('webcomponentsjs');
+    var webComp = Promise.resolve();
+    webComp.then(()=> {
+        if (browserCompatible()) {
+            loadDocument('polymer-elements.html');
+            System.import('aurelia-bootstrapper');
+        } else showCompatMessage();
+    }).catch(err => {
+        console.error(err);
+    });
+}
+
+/**
+ * Configures aurelia application, used by the framework
  * @param aurelia
  */
 export function configure(aurelia) {
-    const MIN_WAIT = 2;
 
     aurelia.use
         .defaultBindingLanguage()
@@ -11,14 +34,34 @@ export function configure(aurelia) {
         .router()
         .eventAggregator();
 
-    if (compatibleBrowser())
-        // boot actions
-        Promise.all([
-            aurelia.start(),
-            wait(MIN_WAIT)
-        ]).then(r => r[0].setRoot('yi/app'));
-    else
-        showCompatMessage();
+    Promise.all([
+        aurelia.start(),
+        minWait
+    ]).then(r => r[0].setRoot('yi/app'));
+}
+
+function loadDocument(url) {
+    return new Promise((resolve) => {
+        var link = document.createElement('link');
+        link.rel = 'import';
+        link.href = url;
+        link.addEventListener('load', resolve);
+        document.head.appendChild(link);
+    });
+}
+
+function browserCompatible() {
+//    return navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+    return supportsWC() && 'observe' in Object;
+}
+
+function showCompatMessage() {
+    minWait.then(()=> {
+        var compatMessage = document.querySelector('#boot>article'),
+            logo = document.querySelector('#logo');
+        compatMessage.classList.add('show');
+        logo.classList.remove('loading');
+    });
 }
 
 function wait(seconds) {
@@ -27,17 +70,9 @@ function wait(seconds) {
     });
 }
 
-function compatibleBrowser() {
-    // is chrome
-    return navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+function supportsWC() {
+    return 'registerElement' in document
+        && 'createShadowRoot' in HTMLElement.prototype
+        && 'import' in document.createElement('link');
 }
 
-function showCompatMessage() {
-    const WAIT = 1;
-    wait(WAIT).then(()=> {
-        var compatMessage = document.querySelector('#boot>article'),
-            logo = document.querySelector('#logo');
-        compatMessage.classList.add('show');
-        logo.classList.remove('loading');
-    });
-}
