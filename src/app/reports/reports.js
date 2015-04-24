65552716ca415bb7b9b1bbfa0058a642cfc218df
Daniel Olano <danielolanoc@gmail.com>
2015-04-24 11:48:37 -0500

    finally got to compile dynamic elements with aurelia

--- src/app/reports/reports.js
+++ src/app/reports/reports.js
@@ -1,12 +1,14 @@
 import {Map} from 'lib/map';
+import {Compiler} from 'lib/util';
 import {BackEnd} from './deleteme-backend';
 import {Router} from 'aurelia-router';
 
 export class Reports {
 
-    static inject() { return [Router, BackEnd, Map]}
+    static inject() { return [Router, BackEnd, Map, Compiler]}
 
-    constructor(router, backend, mainMap) {
+    constructor(router, backend, mainMap, compiler) {
+        this.compiler = compiler;
         this.bE = backend;
         // reports sub router
         this.router = router;
@@ -27,6 +29,8 @@ export class Reports {
             tiles: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
             // tiles: 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'
         };
+        //
+        document.addEventListener('divmarker', (e)=> this.compileMarker(e.detail));
     }
 
     activate(params, queryString, config) {
@@ -41,4 +45,24 @@ export class Reports {
         return this.router.currentInstruction.config.id || '';
     }
 
+    /**
+     * compile the aurelia yi-marker custom element
+     */
+    compileMarker(marker) {
+        var ctx = marker.element.primaryBehavior.boundProperties[0].binding.source;
+        this.compiler.compile(marker.icon, ctx);
+        // workaround to set the lat/lng of the marker correctly
+        // TODO: investigate why it doesn't set them automatically
+        marker.element.latitude = ctx.report.location.lat;
+        marker.element.longitude = ctx.report.location.lng;
+    }
+}
+
+//
+
+function delAttributes(element) {
+    var attrs = Array.prototype.slice.call(element.attributes);
+    for (let attr of attrs) {
+        element.removeAttribute(attr.name)
+    }
 }
