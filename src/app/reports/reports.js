0951240fd4e3e823d0fd0ebc88299ca244d1ae39
Daniel Olano <danielolanoc@gmail.com>
2015-05-06 19:51:04 -0500

    Highlight yi-cards when the corresponding yi-marker is clicked
    fixes #26

--- src/app/reports/reports.js
+++ src/app/reports/reports.js
@@ -4,26 +4,33 @@ import {Map} from 'lib/map';
 import {Compiler} from 'lib/util';
 import {BackEnd} from './deleteme-backend';
 
-@inject(Router, BackEnd, Map, Compiler)
+// mapbox custom map access token
+const MAP_TOKEN = 'pk.eyJ1Ijoib2xhbm9kIiwiYSI6IjdZdV9iTTQifQ.HP-razZKsNmITPgHs4ugIA';
+
+@inject(BackEnd, Map, Compiler)
 export class Reports {
+    // leaflet map configurations
     mapConf = {
         zoomControl: false,
         attributionControl: false,
         minZoom: 5,
-        tiles: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
-        // tiles: 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'
+        // tiles: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
+        tiles: `http://api.tiles.mapbox.com/v4/robjalkh.a4368786/{z}/{x}/{y}.png?access_token=${MAP_TOKEN}`
     };
-
     // search query property
     query = '';
+    // current selected report
+    activeReport = null;
 
-    constructor(router, backend, mainMap, compiler) {
+    constructor(backend, mainMap, compiler) {
         this.compiler = compiler;
         this.bE = backend;
         // Map container
         this.map = mainMap;
         //
-        document.addEventListener('divmarker', (e)=> this.compileMarker(e.detail));
+        document.addEventListener('divmarker', e=> this.compileMarker(e.detail));
+        //
+        document.addEventListener('click', deactivateCardsOnClick);
     }
 
     configureRouter(config, router) {
@@ -36,16 +43,39 @@ export class Reports {
         ]);
     }
 
+    get activeSubSection() {
+        return this.router.currentInstruction.config.id || '';
+    }
+
+    /**
+     * adds selected or not-selected class to the corresponding yi-cards
+     */
+    highlightReportCard(report) {
+        var cards = document.querySelectorAll('yi-card'),
+            list = document.querySelector('yi-card-list'),
+            activeCard;
+
+        // switch classes
+        for (let card of cards) {
+            if (report === card.yiCard.report) {
+                activeCard = card;
+                card.classList.add('selected');
+                card.classList.remove('not-selected');
+            } else {
+                card.classList.add('not-selected');
+                card.classList.remove('selected');
+            }
+        }
+        // scroll to card
+        list.scrollTop = activeCard.offsetTop - (list.offsetHeight/2) + (activeCard.offsetHeight/2)
+    }
+
     activate(params, queryString, config) {
         this.reports = this.bE.getReports();
     }
 
     attached() {
         this.map.mapElement = document.querySelector('leafletMap');
-    }
-
-    get activeSubSection() {
-        return this.router.currentInstruction.config.id || '';
     }
 
     /**
@@ -59,4 +89,13 @@ export class Reports {
         marker.element.latitude = ctx.report.location.lat;
         marker.element.longitude = ctx.report.location.lng;
     }
+}
+
+function deactivateCardsOnClick(e) {
+    var name = e.target.tagName.toLowerCase();
+    if (name !== 'leaflet-map' && name != 'yi-card')
+        for (let card of document.querySelectorAll('yi-card')) {
+            card.classList.remove('selected');
+            card.classList.remove('not-selected');
+        }
 }
